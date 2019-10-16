37aed3e374a07f369461b59029b5a5139add945d
Stuart Lynne <stuart.lynne@gmail.com>
2019-10-15 17:38:45 -0700

    add app_usblan.c

--- firmware/src/app_usblan.c
+++ firmware/src/app_usblan.c
@@ -1,9 +1,6 @@
  
 #include "app.h"
 
-extern APP_DATA appData;
-
-
 //#include "tcpip/tcpip.h"
 
 //#include "app_commands.h"
@@ -47,61 +44,72 @@ void USBDeviceEventHandler ( USB_DEVICE_EVENT event,
 }
 #endif
 
-#if 0
+#if 1
+extern APP_DATA appData;
 void APP_USBDeviceEventHandler(USB_DEVICE_EVENT event, void * eventData, uintptr_t context)
 {   
-    #if 0
+    #if 1
     uint8_t * configurationValue;
-    USB_SETUP_PACKET * setupPacket;
+    //USB_SETUP_PACKET * setupPacket;
     switch(event)
     {
         case USB_DEVICE_EVENT_RESET:
         case USB_DEVICE_EVENT_DECONFIGURED:
 
-            /* Device is reset or deconfigured. Provide LED indication.*/
-            BSP_LEDOn  (APP_USB_LED_1);
-            BSP_LEDOn (APP_USB_LED_2);
-            BSP_LEDOff (APP_USB_LED_3);
+            SYS_PRINT("USB RESET\r\n");
 
-            appData.deviceIsConfigured = false;
+            /* Device is reset or deconfigured. Provide LED indication.*/
+            LED1_Set();
+            LED2_Set();
+            LED3_Clear();
+
+            appData.deviceConfigured = false;
 
             break;
 
         case USB_DEVICE_EVENT_CONFIGURED:
+
+            SYS_PRINT("USB CONFIGURED\r\n");
 
             /* Check the configuration */
             configurationValue = (uint8_t *)eventData;
             if(*configurationValue == 1 )
             {
                 /* The device is in configured state. Update LED indication */
-                BSP_LEDOff  (APP_USB_LED_1);
-                BSP_LEDOff (APP_USB_LED_2);
-                BSP_LEDOn (APP_USB_LED_3);
+                LED1_Clear();
+                LED2_Clear();
+                LED3_Set();
 
                 /* Reset endpoint data send & receive flag  */
-                appData.deviceIsConfigured = true;
+                appData.deviceConfigured = true;
             }
             break;
 
         case USB_DEVICE_EVENT_SUSPENDED:
 
+            SYS_PRINT("USB SUSPENDED\r\n");
+
             /* Device is suspended. Update LED indication */
-            BSP_LEDOff  (APP_USB_LED_1);
-            BSP_LEDOn (APP_USB_LED_2);
-            BSP_LEDOn (APP_USB_LED_3);
+            LED1_Clear();
+            LED2_Set();
+            LED3_Set();
             break;
 
 
         case USB_DEVICE_EVENT_POWER_DETECTED:
+            
+            SYS_PRINT("USB VBUS\r\n");
 
             /* VBUS is detected. Attach the device */
-            //USB_DEVICE_Attach(usbDevHandle);
+            USB_DEVICE_Attach(appData.usbDevHandle);
             break;
 
         case USB_DEVICE_EVENT_POWER_REMOVED:
 
+            SYS_PRINT("USB VBUS LOST\r\n");
+
             /* VBUS is removed. Detach the device */
-            //USB_DEVICE_Detach (usbDevHandle);
+            USB_DEVICE_Detach (appData.usbDevHandle);
             break;
 
 
@@ -116,68 +124,6 @@ void APP_USBDeviceEventHandler(USB_DEVICE_EVENT event, void * eventData, uintptr
     #endif
 }
 #endif
-
-void APP_USBDeviceEventHandler(USB_DEVICE_EVENT event, void * eventData, uintptr_t context)
-{   
-    switch(event)
-    {   
-        case USB_DEVICE_EVENT_RESET:
-        case USB_DEVICE_EVENT_DECONFIGURED:
-            
-            SYS_PRINT("%s: RESET\r\n", __func__);
-            /* Host has de configured the device or a bus reset has happened.
-             * Device layer is going to de-initialize all function drivers.
-             * Hence close handles to all function drivers (Only if they are
-             * opened previously. */
-            
-            LED_Off(); 
-            appData.deviceConfigured = false;
-            //appData.state = APP_STATE_WAIT_FOR_CONFIGURATION;
-            break;
-        
-        case USB_DEVICE_EVENT_CONFIGURED:
-            SYS_PRINT("%s: CONFIGURED\r\n", __func__);
-            LED_On(); 
-            /* Set the flag indicating device is configured. */
-            appData.deviceConfigured = true;
-            
-            /* Save the other details for later use. */
-            appData.configurationValue = ((USB_DEVICE_EVENT_DATA_CONFIGURED*)eventData)->configurationValue;
-            
-            /* Register application HID event handler */
-            //USB_DEVICE_HID_EventHandlerSet(USB_DEVICE_HID_INDEX_0, APP_USBDeviceHIDEventHandler, (uintptr_t)&appData);
-            
-            
-            break;
-        
-        case USB_DEVICE_EVENT_SUSPENDED:
-            SYS_PRINT("%s: SUSPENDED\r\n", __func__);
-            break;
-        
-        case USB_DEVICE_EVENT_POWER_DETECTED:
-            
-            SYS_PRINT("%s: VBUS PowerDetected\r\n", __func__);
-
-            /* VBUS was detected. We can attach the device */
-            
-            USB_DEVICE_Attach (appData.usbDevHandle);
-            break;
-        
-        case USB_DEVICE_EVENT_POWER_REMOVED:
-            SYS_PRINT("%s: VBUS lost\r\n", __func__);
-            LED_Off(); 
-            /* VBUS is not available */
-            USB_DEVICE_Detach(appData.usbDevHandle);
-            break;
-        
-        /* These events are not used in this demo */
-        case USB_DEVICE_EVENT_RESUMED:
-        case USB_DEVICE_EVENT_ERROR:
-        default:
-            break;
-    }
-}
-
 
 
 //USB_DEVICE_HANDLE usbDevHandle;
