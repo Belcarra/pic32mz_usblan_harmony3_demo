ade7627e4a3bae4398d549de1d5f6c93f49d0f65
Stuart Lynne <stuart.lynne@gmail.com>
2019-11-12 13:45:14 -0800

    genmpfs/sh for curiosity on windows

--- firmware/src/config/pic32mz_ef_curiosity/confname.h
+++ firmware/src/config/pic32mz_ef_curiosity/confname.h
@@ -1,5 +1,5 @@
-/* DO NOT EDIT - see src/genmpfs which is called from project properties building */
-
-#define PROJDIR "pic32mz_usblan_harmony3_demo"
-#define CONFNAME "pic32mz_ef_curiosity"
+/* DO NOT EDIT - see src/genmpf.bats which is called from project properties building */
+#define PROJDIR "pic32mz_usblan_harmony3_demo" 
+#define PROJNAME "usblan_demo_pic32mz_ef_curiosity " 
+#define CONFNAME "pic32mz_ef_curiosity" 
 #define DEVICE "PIC32MZ2048EFM100"
--- firmware/src/config/pic32mz_ef_sk/confname.h
+++ firmware/src/config/pic32mz_ef_sk/confname.h
@@ -1,6 +1,5 @@
-/* DO NOT EDIT - see src/genmpfs.bat which is called from project properties building */
-
-#define PROJDIR "pic32mz_usblan_harmony3_demo"
-#define PROJNAME "usblan_demo_pic32mz_ef_sk"
-#define CONFNAME "pic32mz_ef_sk"
+/* DO NOT EDIT - see src/genmpf.bats which is called from project properties building */
+#define PROJDIR "pic32mz_usblan_harmony3_demo" 
+#define PROJNAME "usblan_demo_pic32mz_ef_sk " 
+#define CONFNAME "pic32mz_ef_sk" 
 #define DEVICE "PIC32MZ2048EFM144"
