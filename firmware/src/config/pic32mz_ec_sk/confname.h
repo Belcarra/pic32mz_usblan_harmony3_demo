55cd71ec57db9553201d2ba7eacfd1f62ad26302
Stuart Lynne <stuart.lynne@gmail.com>
2019-12-05 18:49:23 -0800

    pic32mz_ec_sk files

---
+++ firmware/src/config/pic32mz_ec_sk/confname.h
@@ -0,0 +1,6 @@
+/* DO NOT EDIT - see src/genmpfs.bat which is called from project properties building */
+
+#define PROJDIR "pic32mz_usblan_harmony3_demo"
+#define PROJNAME "usblan_demo_pic32mz_ec_sk"
+#define CONFNAME "pic32mz_ec_sk"
+#define DEVICE "PIC32MZ2048ECH144"
